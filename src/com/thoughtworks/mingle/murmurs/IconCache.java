cc27644c7d3d3c42f98fa7f41950dbe4d0daf5c9
Bill DePhillips <bill.dephillips@gmail.com>
2011-07-24 20:25:47 -0700

    fetch icons in background task

--- src/com/thoughtworks/mingle/murmurs/IconCache.java
+++ src/com/thoughtworks/mingle/murmurs/IconCache.java
@@ -2,6 +2,7 @@ package com.thoughtworks.mingle.murmurs;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,12 +13,10 @@ import com.dephillipsdesign.http.HttpClientUtil;
 
 public class IconCache {
 
-  private static final Map<String, String> nameToIconPath = new HashMap<String, String>();
-  private static final Map<String, Bitmap> nameToBitmap = new HashMap<String, Bitmap>();
-
-  private static String getIconPath(String name) {
-    return nameToIconPath.get(name);
-  }
+  private static final Map<String, String> nameToIconPath = new ConcurrentHashMap<String, String>(
+      new HashMap<String, String>());
+  private static final Map<String, Bitmap> nameToBitmap = new ConcurrentHashMap<String, Bitmap>(
+      new HashMap<String, Bitmap>());
 
   private static void setIconPath(String name, String iconPath) {
     if (!nameToIconPath.containsKey(name)) {
@@ -33,24 +32,23 @@ public class IconCache {
   }
 
   private static void queueDownload(String name, String iconPath) {
-    // synchronous for now
-    try {
-      iconPath = Settings.getMingleHost() + iconPath;
-      final Bitmap bitmap = BitmapFactory.decodeStream(HttpClientUtil.openInputStream(iconPath));
-      IconCache.nameToBitmap.put(name, bitmap);
-      Log.d(IconCache.class.getName(),
-          "Successfully stored icon for " + name + ", dimensions: " + bitmap.getWidth() + "x" + bitmap.getHeight());
-    } catch (Exception e) {
-      Log.e(IconCache.class.getName(), "Unable to download icon for " + name + " from " + iconPath, e);
-    }
+    IconDownloader iconDownloader = new IconDownloader();
+    iconDownloader.execute(name, iconPath);
   }
 
   public static class IconDownloader extends AsyncTask<String, Void, Void> {
     protected Void doInBackground(String... params) {
       String name = params[0];
       String iconPath = params[1];
-      final Bitmap bitmap = BitmapFactory.decodeStream(HttpClientUtil.openInputStream(iconPath));
-      IconCache.nameToBitmap.put(name, bitmap);
+      try {
+        iconPath = Settings.getMingleHost() + iconPath;
+        final Bitmap bitmap = BitmapFactory.decodeStream(HttpClientUtil.openInputStream(iconPath));
+        IconCache.nameToBitmap.put(name, bitmap);
+        Log.d(IconCache.class.getName(),
+            "Successfully stored icon for " + name + ", dimensions: " + bitmap.getWidth() + "x" + bitmap.getHeight());
+      } catch (Exception e) {
+        Log.e(IconCache.class.getName(), "Unable to download icon for " + name + " from " + iconPath, e);
+      }
       return null;
     }
   }
